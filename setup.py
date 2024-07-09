8b8b06d5a212dafa89ac84c643e2a5f63b7c2f68
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-09 00:07:25 -0400

    Use tag version to push on PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os 
 
-VERSION = os.environ["PACKAGE_VERSION"] = "0.0.1"
+VERSION = os.environ["PACKAGE_VERSION"] 
 DESCRIPTION = 'Zero-TOTP Database Model'
 LONG_DESCRIPTION = 'The database shared model used accross the zero-totp project.'
 
