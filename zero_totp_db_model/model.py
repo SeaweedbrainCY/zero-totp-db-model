537f4c7d6e2b0de99c281a0b58c6920a240f88fb
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-17 22:39:15 -0500

    Remove admin table

--- zero_totp_db_model/model.py
+++ zero_totp_db_model/model.py
@@ -30,14 +30,8 @@ class TOTP_secret(db.Model):
     __tablename__ = "totp_secret_enc"
     uuid = db.Column(db.String(256), primary_key=True, nullable=False, autoincrement=False)
     user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
-    secret_enc = db.Column(db.Text, nullable=False);
+    secret_enc = db.Column(db.Text, nullable=False)
 
-class Admin(db.Model):
-    __tablename__ = "admin_tokens"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
-    token_hashed = db.Column(db.Text, nullable=False)
-    token_expiration = db.Column(db.String(256), nullable=True, default=None)
 
 
 class Oauth_tokens(db.Model):
